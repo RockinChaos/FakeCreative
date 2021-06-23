1d103c9651f3dc394e0cce0e116e38b498305bc5
RockinChaos <github@CraftationGaming.com>
2021-06-23 03:49:40 -0700

    Prevented /Clearing of Creative Tabs

---
+++ src/me/RockinChaos/fakecreative/listeners/PlayerClear.java
@@ -0,0 +1,48 @@
+/*
+ * FakeCreative
+ * Copyright (C) CraftationGaming <https://www.craftationgaming.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.RockinChaos.fakecreative.listeners;
+
+import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.StringUtils;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import me.RockinChaos.fakecreative.handlers.ItemHandler;
+import me.RockinChaos.fakecreative.handlers.PlayerHandler;
+import me.RockinChaos.fakecreative.handlers.modes.Creative;
+import me.RockinChaos.fakecreative.utils.SchedulerUtils;
+
+public class PlayerClear implements Listener {
+
+   /**
+	* Attempts to remove the player from creative upon exiting the server.
+    * 
+	* @param event - PlayerCommandPreprocessEvent
+	*/
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
+		final String command = event.getMessage();
+		if (PlayerHandler.isFakeCreativeMode(event.getPlayer()) && command != null && !command.isEmpty() && StringUtils.containsIgnoreCase(command, "clear")) {
+			SchedulerUtils.runLater(2L, () -> { 
+				if (ItemHandler.isContentsEmpty(event.getPlayer().getOpenInventory().getTopInventory().getContents())) {
+					Creative.setTabs(event.getPlayer());
+				}
+			});
+		}
+	}
+}
