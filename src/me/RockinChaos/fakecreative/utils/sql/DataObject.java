f78d778d5b6d86a67295f508629dc4c4d0dc600a
RockinChaos <github@CraftationGaming.com>
2021-08-12 00:05:48 -0700

    Added PlayerPreferences

--- src/me/RockinChaos/fakecreative/utils/sql/DataObject.java
+++ src/me/RockinChaos/fakecreative/utils/sql/DataObject.java
@@ -20,16 +20,23 @@ package me.RockinChaos.fakecreative.utils.sql;
 import java.sql.Timestamp;
 
 public class DataObject {
+	
 	private Table table = Table.HOTBAR;
 	private Boolean isTemporary = false;
 	private String playerId = new String();
-	private String worldName = new String();
 	private String position = new String();
+	private Boolean condition = null;
+	private Double numerical = -1.0;
+	private Integer integer = -1;
+	private double health = -1.0;
+	private double maxHealth = -1.0;
+	private int food = -1;
+	private int fireTicks = -101;
 	private String inventory64 = new String();
 	private String timeStamp = new String();
 	
    /**
-    * Gets the Player ID of the DataObject
+    * Gets the Player ID of the DataObject.
     * 
     * @return The Player ID.
     */
@@ -38,22 +45,50 @@ public class DataObject {
 	}
 	
    /**
-    * Gets the World Name of the DataObject
-    * 
-    * @return The World Name.
-    */
-	public String getWorld() {
-		return this.worldName;
-	}
-	
-   /**
-    * Gets the Position of the DataObject
+    * Gets the Position of the DataObject.
     * 
     * @return The Position.
     */
 	public String getPosition() {
 		return this.position;
 	}
+	
+   /**
+    * Gets the Health of the DataObject.
+    * 
+    * @return The Health.
+    */
+	public double getHealth() {
+		return this.health;
+	}
+	
+   /**
+    * Gets the Health Scale of the DataObject.
+    * 
+    * @return The Health Scale.
+    */
+	public double getMaxHealth() {
+		return this.maxHealth;
+	}
+	
+   /**
+    * Gets the Food Level of the DataObject.
+    * 
+    * @return The Food Level.
+    */
+	public int getFood() {
+		return this.food;
+	}
+	
+   /**
+    * Gets the Fire Ticks of the DataObject.
+    * 
+    * @return The Fire Ticks.
+    */
+	public int getFireTicks() {
+		return this.fireTicks;
+	}
+	
    /**
     * Gets the Inventory in 64 Hash of the DataObject
     * 
@@ -64,7 +99,34 @@ public class DataObject {
 	}
 	
    /**
-    * Gets the Time Stamp of the DataObject
+    * Gets the Boolean of the DataObject.
+    * 
+    * @return The Boolean.
+    */
+	public Boolean getBoolean() {
+		return this.condition;
+	}
+	
+   /**
+    * Gets the Double of the DataObject.
+    * 
+    * @return The Double.
+    */
+	public Double getDouble() {
+		return this.numerical;
+	}
+	
+   /**
+    * Gets the Integer of the DataObject.
+    * 
+    * @return The Integer.
+    */
+	public Integer getInt() {
+		return this.integer;
+	}
+	
+   /**
+    * Gets the Time Stamp of the DataObject.
     * 
     * @return The Time Stamp.
     */
@@ -73,16 +135,16 @@ public class DataObject {
 	}
 	
    /**
-    * Sets the Time Stamp of the DataObject
+    * Sets the Time Stamp of the DataObject.
     * 
     * @param stamp - The Time Stamp.
     */
-	public void setTimeStamp(String stamp) {
+	public void setTimeStamp(final String stamp) {
 		this.timeStamp = stamp;
 	}
 
    /**
-    * Gets the Table of the DataObject
+    * Gets the Table of the DataObject.
     * 
     * @return The Table.
     */	
@@ -100,7 +162,7 @@ public class DataObject {
 	}
 	
    /**
-    * Creates a new DataObject instance
+    * Creates a new DataObject instance.
     * 
     * @param table - The Table being accessed.
     * @param isTemporary - If the DataObject is only a reference.
@@ -111,15 +173,37 @@ public class DataObject {
 	}
 	
    /**
-    * Creates a new DataObject instance
+    * Creates a new DataObject instance.
     * 
     * @param table - The Table being accessed.
     * @param playerId - The Player UUID being referenced.
-    * @param worldName - The World Name being referenced.
+    * @param object1 - The Object being referenced.
+    * @param object2 - The Object being referenced.
+    * @param object3 - The Object being referenced.
+    * @param object4 - The Object being referenced.
+    * @param object5 - The Object being referenced.
+    */
+	public DataObject(final Table table, final String playerId, final String object1, final String object2, final String object3, final String object4, final String object5) {
+		this.table = table;
+		this.playerId = playerId;
+		this.health = Double.valueOf(object1);
+		this.maxHealth = Double.valueOf(object2);
+		this.food = Integer.valueOf(object3);
+		this.fireTicks = Integer.valueOf(object4);
+		this.inventory64 = object5;
+		this.timeStamp = new Timestamp(System.currentTimeMillis()).toString();
+	}
+	
+   /**
+    * Creates a new DataObject instance.
+    * 
+    * @param table - The Table being accessed.
+    * @param playerId - The Player UUID being referenced.
     * @param object1 - The Object being referenced.
     * @param object2 - The Object being referenced.
     */
-	public DataObject(Table table, String playerId, String worldName, String object1, String object2) {
+	public DataObject(final Table table, String playerId, final String object1, final String object2) {
+		this.table = table;
 		this.playerId = playerId;
 		this.position = object1;
 		this.inventory64 = object2;
@@ -127,16 +211,65 @@ public class DataObject {
 	}
 	
    /**
-    * Gets the Equal Data of the DataObject
+    * Creates a new DataObject instance.
+    * 
+    * @param table - The Table being accessed.
+    * @param playerId - The Player UUID being referenced.
+    * @param bool - The Boolean being referenced.
+    */
+	public DataObject(final Table table, final String playerId, final boolean bool) {
+		this.table = table;
+		this.playerId = playerId;
+		this.condition = bool;
+		this.timeStamp = new Timestamp(System.currentTimeMillis()).toString();
+	}
+	
+   /**
+    * Creates a new DataObject instance.
+    * 
+    * @param table - The Table being accessed.
+    * @param playerId - The Player UUID being referenced.
+    * @param num - The Double being referenced.
+    */
+	public DataObject(final Table table, final String playerId, final double num) {
+		this.table = table;
+		this.playerId = playerId;
+		this.numerical = num;
+		this.timeStamp = new Timestamp(System.currentTimeMillis()).toString();
+	}
+	
+   /**
+    * Creates a new DataObject instance.
+    * 
+    * @param table - The Table being accessed.
+    * @param playerId - The Player UUID being referenced.
+    * @param num - The Integer being referenced.
+    */
+	public DataObject(final Table table, final String playerId, final int num) {
+		this.table = table;
+		this.playerId = playerId;
+		this.integer = num;
+		this.timeStamp = new Timestamp(System.currentTimeMillis()).toString();
+	}
+	
+   /**
+    * Gets the Equal Data of the DataObject.
     * 
     * @param object1 - The DataObject being compared.
     * @param object2 - The DataObject being compared.
     * @return If the data is equal.
     */	
-	public boolean equalsData(DataObject object1, DataObject object2) {
+	public boolean equalsData(final DataObject object1, final DataObject object2) {
 		if (object1 == null || object2 == null) { return false; }
 		if (object1.getTable().equals(Table.HOTBAR)) {
 			if (object1.getPlayerId().equalsIgnoreCase(object2.getPlayerId()) && (object1.getPosition().equalsIgnoreCase(object2.getPosition()))) {
+				return true;
+			}
+		} else if (object1.getTable().equals(Table.ALLOW_FLIGHT) || object1.getTable().equals(Table.SPEED_FLIGHT) || object1.getTable().equals(Table.SPEED_BREAK) || object1.getTable().equals(Table.SET_FOOD) || 
+				object1.getTable().equals(Table.SET_HEALTH) || object1.getTable().equals(Table.SET_SCALE) || object1.getTable().equals(Table.ALLOW_HUNGER) || object1.getTable().equals(Table.ALLOW_BURN) || 
+				object1.getTable().equals(Table.UNBREAKABLE_ITEMS) || object1.getTable().equals(Table.DROPS_BLOCK) || object1.getTable().equals(Table.SWORD_BLOCK) || object1.getTable().equals(Table.AUTO_RESTORE) || 
+				object1.getTable().equals(Table.SET_GOD) || object1.getTable().equals(Table.DELAY_GOD) || object1.getTable().equals(Table.STORE_INVENTORY) || object1.getTable().equals(Table.PLAYERSTATS)) {
+			if (object1.getPlayerId().equalsIgnoreCase(object2.getPlayerId())) {
 				return true;
 			}
 		}
@@ -144,7 +277,7 @@ public class DataObject {
 	}
 	
    /**
-    * Gets the Removal Values of the DataObject
+    * Gets the Removal Values of the DataObject.
     * 
     * @return The Removal Values.
     */	
@@ -152,22 +285,27 @@ public class DataObject {
 		String removal = "";
 		for (String column : this.table.removal().split(", ")) {
 			if (column.equalsIgnoreCase("Player_UUID")) { removal += "'" + this.getPlayerId() + "',"; }
-			else if (column.equalsIgnoreCase("World_Name")) { removal += "'" + this.getWorld() + "',"; }
 			else if (column.equalsIgnoreCase("Position")) { removal += "'" + this.getPosition() + "',"; }
 		}
 		return removal.substring(0, removal.length() - 1);
 	}
 	
    /**
-    * Gets the Insert Values of the DataObject
+    * Gets the Insert Values of the DataObject.
     * 
     * @return The Insert Values.
     */	
 	public String getInsertValues() {
 		return 
-				  (this.getWorld() != null && !this.getWorld().isEmpty() ? "'" + this.getWorld() + "'," : "")
-				+ (this.getPlayerId() != null && !this.getPlayerId().isEmpty() ? "'" + this.getPlayerId() + "'," : "")
+				  (this.getPlayerId() != null && !this.getPlayerId().isEmpty() ? "'" + this.getPlayerId() + "'," : "")
 			    + (this.getPosition() != null && !this.getPosition().isEmpty() ? "'" + this.getPosition() + "'," : "")
+			    + (this.getBoolean() != null ? "'" + this.getBoolean() + "'," : "")
+			    + (this.getDouble() != null && this.getDouble() >= 0 ? "'" + this.getDouble() + "'," : "")
+			    + (this.getInt() != null && this.getInt() >= 0 ? "'" + this.getInt() + "'," : "")
+			    + (this.getHealth() >= 0 ? "'" + this.getHealth() + "'," : "")
+			    + (this.getMaxHealth() >= 0 ? "'" + this.getMaxHealth() + "'," : "")
+			    + (this.getFood() >= 0 ? "'" + this.getFood() + "'," : "")
+			    + (this.getFireTicks() != -101 ? "'" + this.getFireTicks() + "'," : "")
 			    + (this.getInventory64() != null && !this.getInventory64().isEmpty() ? "'" + this.getInventory64() + "'," : "")
 			    + "'" + new Timestamp(System.currentTimeMillis()) + "'";
 	}
@@ -177,7 +315,23 @@ public class DataObject {
 	* 
 	*/ 
     public enum Table {
-       HOTBAR("`Player_UUID`, `Position`, `Inventory64`, `Time_Stamp`", "Player_UUID, Position");
+    	ALLOW_FLIGHT("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SPEED_FLIGHT("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SPEED_BREAK("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SET_FOOD("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SET_HEALTH("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SET_SCALE("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        ALLOW_HUNGER("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        ALLOW_BURN("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        UNBREAKABLE_ITEMS("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        DROPS_BLOCK("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SWORD_BLOCK("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        AUTO_RESTORE("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        SET_GOD("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        DELAY_GOD("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        STORE_INVENTORY("`Player_UUID`, `Value`, `Time_Stamp`", "Player_UUID"),
+        HOTBAR("`Player_UUID`, `Position`, `Inventory64`, `Time_Stamp`", "Player_UUID, Position"),
+        PLAYERSTATS("`Player_UUID`, `Health`, `Scale`, `Food`, `Fire_Ticks`, `Inventory64`, `Time_Stamp`", "Player_UUID");
     	
     	private String header;
     	private String removal;
